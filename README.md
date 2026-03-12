4f8de31279d8c58b627a84a3eb39d5400005b8da
Franhdez77 <franvier-37@hotmail.com>
2026-03-12 16:57:10 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+ATM WITHDRAWAL SIMULATOR
+
+This is a simple Python program that simulates a basic ATM Withdrawal system. The program starts with an initial account balance and allows the user to withdraw money while checking for invalid or insufficient amounts.
+
+1- The program display a welcome message and shows the account balance. 
+2- The program allows the user to input the amount. 
+3- The program shows error mesagges in case that the amount to withdraw are 0, negative number or bigger than initial value
+4- If the amount is in the correct range, the program display a succesfully message, and this shows the new value in the account  
--- cajero2.py
+++ cajero2.py
@@ -1,79 +1,20 @@
-print ("Bienvenido al cajero")
-contador=0
-valorinicial=1000 
+initial_value=1000 
 
-def inicio():
-    operaciones=int(input("Cuantas operaciones va a realizar: "))
-    if operaciones ==0:
-        return inicio()
-    else:
-        return operaciones
+print ("Welcome to the ATM")
+print (f"You have {initial_value} in your account")
 
-def contraseña ():
-    pin=(int(input("Ecribe la contraseña: ")))
-    if pin==1234:
-        return pin
-    else:
-        print("Contraseña incorrecta")
-        return contraseña()
-    
-contraseña()
-
-def retiro():
-    retirar=float(input("Cuanto desea retirar: "))
-    if retirar > valorinicial:
-        print("Saldo insuficiente")
-        return retiro()
-    elif retirar <=0:
-        print("Valor no disponible")
-        return retiro()
-    else:
-        return retirar
-    
-
-def deposito():
-    depositar=float(input("Cuanto desea depositar: "))
-    if depositar > valorinicial:
-        print("Saldo insuficiente")
-        return retiro()
-    elif depositar<=0:
-        print("Valor no disponible")
-        return retiro()
-    else:
-        return depositar
-       
+withdraw= float(input("How much you want to withdraw?: "))
 
 
+if withdraw > initial_value:
+    print("Insufficient founds")
 
-inicios=inicio()
-
-while contador<inicios:
-        
-        print("1-Consultar saldo")
-        print("2-Retirar dinero")
-        print("3-Depositar dinero")
-        print("4-Salir")
-        menu=int(input("selecciona una opcion: "))
-        
-        if menu ==1:
-            print ("su saldo es", valorinicial)
-            contador+=1
-        elif menu ==2:
-             retirar=retiro()
-             valorinicial -= retirar
-             print("su nuevo saldo es ",valorinicial)
-             contador+=1
-        elif menu==3:
-             depositar=deposito()
-             valorinicial += depositar
-             print("su nuevo saldo es: ",valorinicial)
-        elif menu ==4:
-            print("gracias por usar el cajero")
-            break
-        else: 
-            print("Opcion no valida. Intente nuevamente")
-
-
+elif withdraw <=0:
+    print("Invalid amount")
+else:
+    new_value= initial_value-withdraw
+    print("Your withdraw have been succesfully")
+    print(f"Now you have {new_value} dollars in your account  ")
 
 
 
